1e34697356d370e7a8c7bcd523e57e73b78e6231
MehakSankhla <75704051+MehakSankhla@users.noreply.github.com>
2021-01-12 17:03:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function spanelem(){
+window.alert("Wishing you a fun-filled holiday season and best wishes for a happy New Year! I hope you enjoy yourself, and this year will be better. Happy Holidays!")
+}
